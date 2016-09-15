90ac88dfd778b5c55f6eae2dfe85fdc0d67d0d77
eqgao <215160083@qq.com>
2016-09-15 17:17:31 +0800

    upgrade test

---
+++ app/src/main/java/com/example/helloworld/moeset/engine/BCYDataRequest.java
@@ -0,0 +1,49 @@
+package com.example.helloworld.moeset.engine;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by helloworld on 2016/9/15.
+ */
+
+public class BCYDataRequest {
+    private String mUrl;
+    private Context context;
+    private String mCookies;
+
+    private void login() {
+
+
+    }
+
+    private void saveBcyCookies() {
+        SharedPreferences.Editor write = context.getSharedPreferences("config", Context.MODE_PRIVATE).edit();
+        write.putString("BcyCookies", mCookies);
+        write.commit();
+    }
+
+    private String loadBcyCookies() {
+        SharedPreferences read = context.getSharedPreferences("config", Context.MODE_PRIVATE);
+        return read.getString("BcyCookies", "");
+    }
+    private void getCookies(){
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, mUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+
+}
